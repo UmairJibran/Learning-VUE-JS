7c80ff84cc2e740c70b9cad832c8ac4dc7ac686b
Umair Jibran <umairjibran7@gmail.com>
2021-03-27 23:29:58 +0500

    complete 04_04

---
+++ vue-js-developer-path/Course 1. Learning Vue.js/4. Vue Components/gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require("gulp"),
+	browserSync = require("browser-sync").create(),
+	postcss = require("gulp-postcss"),
+	source = "./src/",
+	dest = "./build";
+
+function html() {
+	return gulp.src(dest + "**/*.html");
+}
+
+function styles() {
+	return gulp
+		.src(source + "**/*.css")
+		.pipe(postcss([require("tailwindcss"), require("autoprefixer")]))
+		.pipe(gulp.dest(dest + "/css"));
+}
+
+function watch() {
+	gulp.watch(source + "css/**/*.css", styles).on("change", browserSync.reload);
+	gulp.watch(source + "*.css", styles).on("change", browserSync.reload);
+	gulp.watch(dest + "/*.html", html).on("change", browserSync.reload);
+	gulp
+		.watch(
+			["./tailwind.config.js", "./postcss.config.js", "./gulpfile.js"],
+			styles
+		)
+		.on("change", browserSync.reload);
+}
+
+function server() {
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: dest,
+		},
+	});
+	watch();
+}
+
+var build = gulp.series(gulp.parallel(styles, html), server, watch);
+
+gulp.task("default", build);
